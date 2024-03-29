0f4977973790104d908232317565cae2efc8cf65
camilomdeoca <camilomdeoca@gmail.com>
2024-03-29 02:28:28 -0300

    fix: the number of floats in vertex buffer was written in file instead of number of vertices

--- to-paca-format/model.cpp
+++ to-paca-format/model.cpp
@@ -3,6 +3,17 @@
 
 #include <fstream>
 
+uint32_t vertexTypeToNumOfFloats(VertexType::Type type)
+{
+    switch (type) {
+        case VertexType::float3pos_float3norm_float2texture: return 3+3+2;
+        case VertexType::float3pos_float3norm_float3tang_float2texture: return 3+3+3+2;
+        default: break;
+    }
+
+    exit(1);
+}
+
 void saveModel(const std::string &name, const Model &model, const std::string &filename)
 {
     std::ofstream outfile(filename, std::ios::binary);
@@ -17,7 +28,7 @@ void saveModel(const std::string &name, const Model &model, const std::string &f
     {
         MeshSubHeader subheader;
         subheader.vertexType = mesh.vertexType;
-        subheader.vertexCount = mesh.vertices.size();
+        subheader.vertexCount = mesh.vertices.size()/vertexTypeToNumOfFloats(VertexType::Type(mesh.vertexType));
         subheader.indexType = IndexType::int32bit;
         subheader.indexCount = mesh.indices.size();
         subheader.materialLength = mesh.materialName.size();
